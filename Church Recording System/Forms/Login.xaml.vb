eda76b8769b43b5fd178d0c2eddfc18735d8d8f9
Jeffrey A. Reyes <jeofadriel@hotmail.com>
2013-09-23 14:28:27 +0700

    Added source files

---
+++ Church Recording System/Forms/Login.xaml.vb
@@ -0,0 +1,9 @@
+﻿
+Imports System.Data.OleDb
+
+Public Class Login
+
+    Private Sub cmdCancel_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles cmdCancel.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Church Recording System/Forms/MainWindow.xaml.vb
@@ -0,0 +1,19 @@
+﻿
+Imports System.Data.OleDb
+
+Class MainWindow
+
+    Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As System.Windows.RoutedEventArgs) Handles Me.Loaded
+
+        'hide buttons
+        With Me
+            .cmdBaptismal.Visibility = Windows.Visibility.Hidden
+            .cmdConfirmation.Visibility = Windows.Visibility.Hidden
+        End With
+        Dim newwindow As New Login
+
+        With newwindow
+            .ShowDialog()
+        End With
+    End Sub
+End Class
